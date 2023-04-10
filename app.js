423a0ceacb55b62800e3deec8a50cf322c57fc48
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-04-10 23:13:33 +0530

    mc

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.set('trust proxy', 1);
 app.use(
   rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 15 * 60 * 1000, // 15 mnt
     max: 100, 
   })
 );
