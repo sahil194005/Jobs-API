d438cd119f15c70a3b9e0223ea974ba1efc338c6
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-04-10 23:12:08 +0530

    minor changes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('trust proxy', 1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 100, 
   })
 );
 
